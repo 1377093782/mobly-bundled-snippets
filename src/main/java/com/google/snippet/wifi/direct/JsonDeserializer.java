7a7e84cf71e22f1a22aa1b6960b3d2373f11bd12
An Liu <aaanliu@google.com>
2024-12-24 02:28:13 +0000

    1

--- src/main/java/com/google/snippet/wifi/direct/JsonDeserializer.java
+++ src/main/java/com/google/snippet/wifi/direct/JsonDeserializer.java
@@ -18,6 +18,7 @@ package com.google.snippet.wifi.direct;
 
 import android.net.MacAddress;
 import android.net.wifi.p2p.WifiP2pConfig;
+import android.net.wifi.p2p.nsd.WifiP2pServiceRequest;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -26,12 +27,13 @@ import org.json.JSONObject;
 public class JsonDeserializer {
     private static final String PERSISTENT_MODE = "persistent_mode";
     private static final String DEVICE_ADDRESS = "device_address";
-    private static final String GROUP_CLIENT_IP_PROVISIONING_MODE =
-            "group_client_ip_provisioning_mode";
+    private static final String GROUP_CLIENT_IP = "group_client_ip_provisioning_mode";
     private static final String GROUP_OPERATING_BAND = "group_operating_band";
     private static final String GROUP_OPERATING_FREQUENCY = "group_operating_frequency";
     private static final String NETWORK_NAME = "network_name";
     private static final String PASSPHRASE = "passphrase";
+    private static final String PROTOCOL_TYPE = "protocol_type";
+
 
     /** Converts Python dict to android.net.wifi.p2p.WifiP2pConfig. */
     public static WifiP2pConfig jsonToWifiP2pConfig(JSONObject jsonObject) throws JSONException {
@@ -53,9 +55,8 @@ public class JsonDeserializer {
         if (jsonObject.has(DEVICE_ADDRESS)) {
             builder.setDeviceAddress(MacAddress.fromString(jsonObject.getString(DEVICE_ADDRESS)));
         }
-        if (jsonObject.has(GROUP_CLIENT_IP_PROVISIONING_MODE)) {
-            builder.setGroupClientIpProvisioningMode(
-                    jsonObject.getInt(GROUP_CLIENT_IP_PROVISIONING_MODE));
+        if (jsonObject.has(GROUP_CLIENT_IP)) {
+            builder.setGroupClientIpProvisioningMode(jsonObject.getInt(GROUP_CLIENT_IP));
         }
         if (jsonObject.has(GROUP_OPERATING_BAND)) {
             builder.setGroupOperatingBand(jsonObject.getInt(GROUP_OPERATING_BAND));
@@ -70,5 +71,20 @@ public class JsonDeserializer {
             builder.setPassphrase(jsonObject.getString(PASSPHRASE));
         }
         return builder.build();
+    }
+
+    /**
+     * Converts Python dict to android.net.wifi.p2p.nsd.WifiP2pServiceRequest.
+     */
+    public static WifiP2pServiceRequest jsonToWifiP2pServiceRequest(JSONObject jsonObject)
+            throws JSONException {
+        if (jsonObject == null) {
+            throw new JSONException("jsonObject is null,please call jsonToWifiP2pServiceRequest"
+                    + " with a valid JSONObject");
+        }
+        if (jsonObject.has(PROTOCOL_TYPE)) {
+            return WifiP2pServiceRequest.newInstance(jsonObject.getInt(PROTOCOL_TYPE));
+        }
+        return null;
     }
 }
